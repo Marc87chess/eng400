5c950ada8c0c64b3fb61f3d1a981fe4aaaa5e083
marc87chess <marcdurieu87@gmail.com>
2026-02-08 11:59:28 -0700

    DONE, V1

--- Good_code.py
+++ Good_code.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from Utils import build_tan_pwl, make_deviation_model, bin_index
 
 class LQRController:
-    def __init__(self,Q,R,A_bank = None,B_bank = None):
+    def __init__(self,Q,R,A_bank = None,B_bank = None,state_space = None):
+        self.state_space = state_space
         self.K_bank = None
         self.K_bank_descrete = None
         self.Q = Q
@@ -47,13 +48,30 @@ class LQRController:
         return self.K_bank
     
 
+    def lqr_controller_from_devbank(self,x):
+        TORQUE_LIM = 50.0
+        # pick model cell based on *current* roll/pitch (or whatever you’re indexing)
+        ip = bin_index(x[6], self.state_space.edges_phi)
+        it = bin_index(x[7], self.state_space.edges_th)
+
+        x0, u0 = self.state_space.x0_bank[ip, it], self.state_space.u0_bank[ip, it]      # equilibrium for that cell
+        K = self.K_bank[ip, it]              # 4x12
+
+        dx = x - x0
+        du = -K @ dx
+        u = u0 + du
+
+
+        return np.clip(u, -TORQUE_LIM, TORQUE_LIM)
+        
+
 
 
     
     
 class State_space:
     def __init__(self,Nphi=30, Ntheta=30,phi_max=np.deg2rad(35), theta_max=np.deg2rad(35),g=9.81, m=2.0, Ix=0.2, Iy=0.2, Iz=0.2,b_motion=-0.1,b_spinning=-0.1,states=12, 
-                 inputs=4):
+                 inputs=4, outputs=6):
         self.Nphi = Nphi
         self.Ntheta = Ntheta    
         self.phi_max = phi_max
@@ -81,6 +99,9 @@ class State_space:
         self.b_th = None
         self.states = states
         self.inputs = inputs
+        self.n_outputs = outputs
+        self.C = np.zeros((self.n_outputs, self.states))
+        self.D = np.zeros((self.n_outputs, self.inputs))
     def gen_banks(self):
             
         # Separate tables for phi and theta
@@ -101,6 +122,23 @@ class State_space:
         self.b_phi = b_phi
         self.a_th = a_th
         self.b_th = b_th
+
+
+       
+
+        # velocities
+        self.C[0, 3] = 1.0   # vx = xdot
+        self.C[1, 4] = 1.0   # vy = ydot
+        self.C[2, 5] = 1.0   # vz = zdot
+
+        # angles
+        self.C[3, 6] = 1.0   # phi
+        self.C[4, 7] = 1.0   # theta
+        self.C[5, 8] = 1.0   # psi
+
+        # No direct feedthrough
+        self.D = np.zeros((self.n_outputs, self.inputs))
+
         for ip in range(self.Nphi):
             for it in range(self.Ntheta):
                 A = np.zeros((self.states, self.states))
@@ -182,7 +220,8 @@ class Virtual_input_to_motor_inputs:
         Minv = np.linalg.inv(M)
         self.Minv = Minv
         return Minv, M
-    def mix_to_motors(self, Minv,destroy=False,iters=4):
+    def mix_to_motors(self,u, Minv,destroy=False,iters=4):
+        self.virtual_u = u
         self.f = Minv @ self.virtual_u
         if destroy:
             for _ in range(max(1, iters)):
@@ -207,9 +246,9 @@ class Virtual_input_to_motor_inputs:
                     if abs(dz[k]) > 1e-12:
                         self.virtual_u[3] += under[k] / dz[k]
 
-        f = np.clip(self.f, self.f_min, self.f_max)
-        self.u = self.M @ f
-        return self.u
+        self.f = np.clip(self.f, self.f_min, self.f_max)
+        self.u = self.M @ self.f
+        return self.u,self.f
 
 class Simulator:
     def __init__(self,LQR_controller = None,State_space = None,virtual_input_to_motor_inputs = None):
@@ -218,10 +257,18 @@ class Simulator:
         self.virtual_input_to_motor_inputs = virtual_input_to_motor_inputs
         self.bank_a = self.State_space.abank
         self.bank_b = self.State_space.bbank
-        self.dev_bank = self.State_space.dev_bank
+        self.x0_bank = self.State_space.x0_bank
+        self.u0_bank = self.State_space.u0_bank
+        self.K_bank = self.LQR_controller.K_bank
         self.edges_phi = self.State_space.edges_phi
         self.edges_th = self.State_space.edges_th
-        self.current_state = np.zeros(self.State_space.n_states)
+        self.current_state = np.zeros(self.State_space.states)
+        self.next_state = np.zeros(self.State_space.states)
+        self.y = np.zeros(self.State_space.n_outputs)
+        self.x_hist = None
+        self.y_hist = None
+        self.u_hist = None
+        self.f_hist = None
         self.current_deviation = None
         
     def step_state(self, u, dt):
@@ -232,7 +279,7 @@ class Simulator:
         # 2) get deviation model + trim
         A = self.bank_a[ip, it]
         B = self.bank_b[ip, it]
-        self.current_deviation, u0 = self.dev_bank[ip, it]
+        self.current_deviation, u0 = self.x0_bank[ip, it], self.u0_bank[ip, it]
 
         # 3) deviation coordinates
         dx = self.current_state - self.current_deviation
@@ -249,4 +296,112 @@ class Simulator:
         self.current_state = x_next
 
         return x_next
+    
 
+    def simulator(self,dt,deviation_output):
+        # step
+        self.x_next = self.step_state(self.virtual_input_to_motor_inputs.u, dt=dt)
+        # output in deviation coordinates: y = C*dx + D*du
+        ip = bin_index(self.x_next[6], self.edges_phi)
+        it = bin_index(self.x_next[7], self.edges_th)
+        x0, u0 = self.x0_bank[ip, it], self.u0_bank[ip, it]
+        if deviation_output:
+
+            dx = self.x_next - x0
+            du = self.virtual_input_to_motor_inputs.u - u0
+            y = self.State_space.C @ dx + self.State_space.D @ du
+        else:
+            # physical outputs directly from state
+            y = self.State_space.C @ self.x_next + self.State_space.D @ self.virtual_input_to_motor_inputs.u
+
+        return self.x_next, y,x0, u0
+    
+    def run_and_plot(self,T=5.0, dt=0.01, deviation_output=False):
+        """
+        x0: initial 12-state vector
+        u_fun: function u_fun(t, x) -> (4,) input vector
+        """
+        self.x_next, self.y, x0, u0 = self.simulator(dt=dt, deviation_output=deviation_output)
+
+        N = int(np.floor(T / dt)) + 1
+        t = np.linspace(0.0, T, N)
+
+        self.x_hist = np.zeros((N, self.State_space.states))
+        self.y_hist = np.zeros((N, self.State_space.n_outputs))
+        self.u_hist = np.zeros((N, self.State_space.inputs))
+        self.f_hist = np.zeros((N, self.State_space.inputs))
+        x = np.array(x0, dtype=float)
+        thrust_disturbance = np.random.uniform(0,10)
+        tau_z_disturbance= np.random.uniform(0,0.5)
+        tau_y_disturbance= np.random.uniform(0,0.5)
+        tau_x_disturbance= np.random.uniform(0,0.5)
+        distrubance = np.array([thrust_disturbance, tau_x_disturbance, tau_y_disturbance, tau_z_disturbance])
+        
+        for k in range(N):
+            if t[k] < 1.0:
+                u = distrubance
+                _, f = self.virtual_input_to_motor_inputs.mix_to_motors(u, self.virtual_input_to_motor_inputs.Minv, destroy=True, iters=8)
+            else:
+                u = self.LQR_controller.lqr_controller_from_devbank(x)
+                u,f = self.virtual_input_to_motor_inputs.mix_to_motors(u, self.virtual_input_to_motor_inputs.Minv, destroy=True, iters=8)
+            self.x_hist[k] = x
+            self.f_hist[k] = f
+            self.u_hist[k] = u
+
+            x, y, x0, u0 = self.simulator(dt=dt, deviation_output=deviation_output)
+            self.y_hist[k] = y
+
+        labels = ["vx", "vy", "vz", "phi", "theta", "psi"]
+
+        plt.figure()
+        for i in range(6):
+            plt.plot(t, self.y_hist[:, i], label=labels[i])
+        plt.xlabel("time (s)")
+        plt.ylabel("outputs")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+        # optional: also plot inputs
+        plt.figure()
+        for i, name in enumerate(["u1(net z)", "tau_x", "tau_y", "tau_z"]):
+            plt.plot(t, self.u_hist[:, i], label=name)
+        plt.xlabel("time (s)")
+        plt.ylabel("inputs")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+        for i, name in enumerate(["f1", "f2", "f3", "f4"]):
+            plt.plot(t, self.f_hist[:, i], label=name)
+        plt.xlabel("time (s)")
+        plt.ylabel("inputs")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+        return t, self.x_hist, self.y_hist, self.u_hist
+
+      
+
+if __name__ == "__main__":
+    # Example usage
+    Q = np.diag([
+    0.0, 0.0, 0.0,     # position (ignore)
+    10.0, 10.0, 10.0,# vx, vy, vz (CARE A LOT)
+    5.0, 5.0, 0.2,     # roll, pitch, yaw (small, not zero)
+    5.0, 5.0, 1.0      # p, q, r rates (small-moderate damping)
+    ])
+    
+    R = np.diag([1.0, 0.2, 0.2, 0.3])
+
+    Virtual_input_to_motor_inputs = Virtual_input_to_motor_inputs(virtual_u=np.zeros(4))
+    Virtual_input_to_motor_inputs.mixing_matrix_gen()
+    State_space = State_space()
+    State_space.gen_banks()
+    lqr_controller = LQRController(Q,R,state_space=State_space)
+    lqr_controller.load_banks(Q,R,State_space.abank,State_space.bbank)
+    K_bank = lqr_controller.gen_K_bank_continous()
+    Simulator = Simulator(LQR_controller=lqr_controller,State_space=State_space,virtual_input_to_motor_inputs=Virtual_input_to_motor_inputs)    
+    Simulator.run_and_plot()
+
+    print("K_bank shape:", K_bank.shape)
